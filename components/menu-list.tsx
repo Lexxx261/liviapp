2ca31181a4a25bceb83c3bd4de8973f82d3b727a
Leonel Quintana <leonelquint@gmail.com>
2025-02-17 15:51:53 -0300

    cambio en menulist

--- components/menu-list.tsx
+++ components/menu-list.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
 import Image from "next/image"
-import logo from "./assets/logos/logo-n.png"
 import { useRouter } from "next/navigation"
 
 import {
@@ -17,74 +16,7 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
-export function MenuList() {
-  const router = useRouter()
-
-  return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Sobre Nosotros</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <Link
-                    href="/"
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                  >
-                    <Image src={logo} alt="Logo" width={100} height={100} onClick={() => router.push("/")} />
-                    <div className="mb-2 mt-4 text-lg font-bold">
-                      Livi App
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Una solución integral para crear y gestionar sitios web, landing pages, e-commerce y blogs de forma fácil y eficiente.
-                    </p>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/" title="Tienda">
-                Accede a toda tu información, pedidos y más
-              </ListItem>
-              <ListItem href="/" title="Ofertas">
-                Sección dedicada a promociones y descuentos especiales
-              </ListItem>
-              <ListItem href="/" title="Accesorios">
-                Productos complementarios
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Categorias</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/accessories" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Accesorios
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  )
-}
-
-export default MenuList
-
+// Lista de categorías antes de su uso
 const components: { title: string; href: string; description: string }[] = [
   {
     title: "Pantalón",
@@ -103,15 +35,16 @@ const components: { title: string; href: string; description: string }[] = [
   },
 ]
 
+// Componente ListItem antes de su uso
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, href, ...props }) => {
+>(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link
-          href={href}
+        <a
+          ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
@@ -122,10 +55,75 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
+        </a>
       </NavigationMenuLink>
     </li>
   )
 })
 ListItem.displayName = "ListItem"
 
+export function MenuList() {
+  const router = useRouter()
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Sobre Nosotros</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <div className="cursor-pointer" onClick={() => router.push("/")}>
+                      <Image src="/assets/logos/logo-n.png" alt="Logo" width={100} height={100} />
+                    </div>
+                    <div className="mb-2 mt-4 text-lg font-bold">Livi App</div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Una solución integral para crear y gestionar sitios web, landing pages, e-commerce y blogs de forma fácil y eficiente.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/shop" title="Tienda">
+                Accede a toda tu información, pedidos y más
+              </ListItem>
+              <ListItem href="/offers" title="Ofertas">
+                Sección dedicada a promociones y descuentos especiales
+              </ListItem>
+              <ListItem href="/" title="Accesorios">
+                Productos complementarios
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Categorías</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {components.map((component) => (
+                <ListItem key={component.title} title={component.title} href={component.href}>
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Accesorios
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+export default MenuList
